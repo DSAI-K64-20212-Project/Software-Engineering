314ba42c8258a6d77448e4ebcc5061c1ff888aa9
khanh <khanh.techbk@gmail.com>
2022-07-16 15:58:29 +0700

    update Hoadon

---
+++ src/project/UI/HoaDon.java
@@ -0,0 +1,13 @@
+package project.UI;
+
+public class HoaDon {
+    private String mahoadon;
+
+    public String getMahoadon() {
+        return mahoadon;
+    }
+
+    public void setMahoadon(String mahoadon) {
+        this.mahoadon = mahoadon;
+    }
+}
